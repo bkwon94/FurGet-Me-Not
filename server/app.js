3475224e9d4376600ed0569de16ba0838f4c1bb1
Brian Kwon <bkwon94@gmail.com>
2019-05-04 14:48:43 -0400

    Basic file structure complete, installed core dependencies

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/../public')));
+
+
+
+module.exports = app;
